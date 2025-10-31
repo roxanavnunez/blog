8daa16008d97da85a5d63e3ad368705a945984fd
Roxana Nuñez <roxana.nunez.nav@gmail.com>
2025-10-30 22:03:12 -0600

    feat(blogs.models): Add Blog and Post models

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    """An overall blog."""
+    title = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_last_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """Return a string representation of the blog"""
+        return self.title
+
+class Post(models.Model):
+    """Individual blog post"""
+    title = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_last_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """Return a simple string representing the post."""
+        if len(self.text) > 50:
+            return f"{self.text[:50]}..."
+        return f"{self.text}"
