e97a36951c2d49cc38ddc80b78bd1937b58fe2cc
Roxana Nuñez <roxana.nunez.nav@gmail.com>
2025-10-31 21:19:49 -0600

    feat(models): Update models

--- blogs/models.py
+++ blogs/models.py
@@ -1,24 +1,25 @@
 from django.db import models
 
-class Blog(models.Model):
+class Topic(models.Model):
     """An overall blog."""
-    title = models.CharField(max_length=200)
+    topic = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
     date_last_modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         """Return a string representation of the blog"""
-        return self.title
+        return self.topic
 
 class Post(models.Model):
     """Individual blog post"""
-    title = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
     date_last_modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         """Return a simple string representing the post."""
-        if len(self.text) > 50:
-            return f"{self.text[:50]}..."
-        return f"{self.text}"
+        if len(self.title) > 50:
+            return f"{self.title[:50]}..."
+        return f"{self.title}"
