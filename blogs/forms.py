c8cdc4d8ac5b17b257a634b86f4aee88e1f0e6d8
Roxana Nuñez <roxana.nunez.nav@gmail.com>
2025-11-05 21:45:03 -0600

    feat(blogs/forms): Add form to edit a post

---
+++ blogs/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'text']
+        widgets = {
+            'title': forms.TextInput(attrs={'size': 50}),
+            'text': forms.Textarea(attrs={'rows':10, 'cols': 50}),
+        }
