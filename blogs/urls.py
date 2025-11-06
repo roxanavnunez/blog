7f5f06b2825b9b09b64daef45fc5b48ab0736bef
Roxana Nuñez <roxana.nunez.nav@gmail.com>
2025-11-05 21:45:52 -0600

    feat(blogs/urls): Add URL to edit a post

--- blogs/urls.py
+++ blogs/urls.py
@@ -7,10 +7,15 @@ app_name = 'blogs'
 urlpatterns = [
     # Home page
     path('', views.index, name='index'),
+    
     # Page to show all topics
     path('topics/', views.topics, name='topics'),
     # Page to show individual topic 
     path('topic/<int:topic_id>/', views.topic, name='topic'),
     # Page to show the content of a post
     path('post/<int:post_id>', views.post, name='post'),
+    
+    # Page to edit a post
+    path('post/<int:post_id>/edit/', views.edit_post, name='edit_post'),
+
 ]
